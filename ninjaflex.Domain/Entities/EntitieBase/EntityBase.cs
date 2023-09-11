12f4a45fe542f5c84f6dbf26f93754e38628c434
Matheus Ribeiro <omatheusribeiro@outlook.com>
2023-09-10 22:54:24 -0300

    Populated domain layer with base entity, users, and user types

---
+++ ninjaflex.Domain/Entities/EntitieBase/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace ninjaflex.Domain.Entities.EntitieBase
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public int UserCreation { get; set; }
+        public int? UserAlteration { get; set; }
+        public int? UserExclusion { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime? DateAlteration { get; set; }
+        public DateTime? DateExclusion { get; set; }
+    }
+}
---
+++ ninjaflex.Domain/Entities/UserType.cs
@@ -0,0 +1,10 @@
+﻿using ninjaflex.Domain.Entities.EntitieBase;
+
+namespace ninjaflex.Domain.Entities
+{
+    public class UserType : EntityBase
+    {
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
